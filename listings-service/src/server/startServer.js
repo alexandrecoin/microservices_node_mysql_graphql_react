e20f644e07f49e69b74458f634bd1eda76b12369
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-01-05 21:15:31 +0100

    (Server): Get DB data for listings-service

--- listings-service/src/server/startServer.js
+++ listings-service/src/server/startServer.js
@@ -3,6 +3,8 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import accessEnv from '#root/helpers/accessEnv';
 
+import setupRoutes from './routes';
+
 const PORT = accessEnv('PORT', 7100);
 
 const app = express();
@@ -15,6 +17,8 @@ app.use(
     credentials: true,
   }),
 );
+
+setupRoutes(app);
 
 app.listen(PORT, '0.0.0.0', () => {
   console.info(`Listings service listening on port ${PORT}`);
