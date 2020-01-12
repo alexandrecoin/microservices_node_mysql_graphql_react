999ec6b5f4aacf677c2c617ca809f3efbfc6fe62
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-01-12 17:07:01 +0100

    Added basic layout

--- front-end/src/components/Root/Root.js
+++ front-end/src/components/Root/Root.js
@@ -1,7 +1,39 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  box-sizing: border-box;
+  height: 100%;
+  padding: 1rem;
+  width: 100%;
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-row: row nowrap;
+  margin: 0 auto;
+  width: 80rem;
+`;
+
+const Content = styled.div`
+  flex: 1;
+  margin-right: 1rem;
+`;
+
+const Sidebar = styled.div`
+  flex: 0 auto;
+  widht: 10rem;
+`;
 
 const Root = () => {
-  return <h1>Root</h1>;
+  return (
+    <Wrapper>
+      <Container>
+        <Content>ok</Content>
+        <Sidebar>zefzfec</Sidebar>
+      </Container>
+    </Wrapper>
+  );
 };
 
 export default Root;
