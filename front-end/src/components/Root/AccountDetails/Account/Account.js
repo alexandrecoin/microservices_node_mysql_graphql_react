343fda96ee5e31a2564a2c6068b3873be220b58c
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-01-22 17:52:33 +0100

    (Front): Complete logout

--- front-end/src/components/Root/AccountDetails/Account/Account.js
+++ front-end/src/components/Root/AccountDetails/Account/Account.js
@@ -1,5 +1,11 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+
+import { useMutation } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { clearSession } from '#root/store/ducks/session';
+
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -13,12 +19,35 @@ const Email = styled.div`
   margin-top: 0.25rem;
 `;
 
+const LogOutLink = styled.a.attrs({ href: '#' })`
+  color: blue;
+  display: block;
+  margin-top: 0.25rem;
+`;
+
+const mutation = gql`
+  mutation($sessionId: ID!) {
+    deleteUserSession(sessionId: $sessionId)
+  }
+`;
+
 const Account = () => {
+  const dispatch = useDispatch();
+  const [deleteUserSession] = useMutation(mutation);
   const session = useSelector((state) => state.session);
 
   return (
     <Wrapper>
       Logged in as <Email>{session.user.email}</Email>
+      <LogOutLink
+        onClick={(event) => {
+          event.preventDefault();
+          dispatch(clearSession());
+          deleteUserSession({ variables: { sessionId: session.id } });
+        }}
+      >
+        Logout
+      </LogOutLink>
     </Wrapper>
   );
 };
