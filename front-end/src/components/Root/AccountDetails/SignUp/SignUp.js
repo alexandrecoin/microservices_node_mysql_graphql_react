272150467e06cd30142a54fafb8005738bce8c24
Alexandre Coin <alexandre.pc.coin@gmail.com>
2020-01-24 09:15:58 +0100

    (Front): Signup form

--- front-end/src/components/Root/AccountDetails/SignUp/SignUp.js
+++ front-end/src/components/Root/AccountDetails/SignUp/SignUp.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import { useMutation } from '@apollo/react-hooks';
-import { useDispatch } from 'react-redux';
-import { setSession } from '#root/store/ducks/session';
 import gql from 'graphql-tag';
 import useForm from 'react-hook-form';
 import styled from 'styled-components';
+import * as yup from 'yup';
 
 import TextInput from '#root/components/shared/TextInput';
 
@@ -21,41 +20,50 @@ const LabelText = styled.strong`
   margin-bottom: 0.25rem;
 `;
 
-const LoginButton = styled.button`
+const SignUpButton = styled.button`
   display: inline-block;
   margin-top: 0.5rem;
 `;
 
-const OrSignUp = styled.span`
+const OrLogin = styled.span`
   font-size: 0.9rem;
 `;
 
 const mutation = gql`
   mutation($email: String!, $password: String!) {
-    createUserSession(email: $email, password: $password) {
+    createUser(email: $email, password: $password) {
       id
-      user {
-        email
-        id
-      }
     }
   }
 `;
 
-const Login = ({onChangeToSignUp: pushChangeToSignUp}) => {
-  const dispatch = useDispatch();
-  const [createUserSession] = useMutation(mutation);
+const validationSchema = yup.object().shape({
+  email: yup.string().required(),
+  password: yup
+    .string()
+    .required()
+    .test(
+      'sameAsConfirmPassword',
+      '${path} is not the same as confirmation password',
+      function () {
+        return this.parent.password === this.parent.confirmPassword;
+      },
+    ),
+});
+
+const SignUp = ({ onChangeToLogin: pushChangeToLogin }) => {
+  const [createUser] = useMutation(mutation);
   const {
-    formState: { isSubmitting },
+    formState: { isSubmitting, isValid },
     handleSubmit,
     register,
-  } = useForm();
+    reset
+  } = useForm({ mode: 'onChange', validationSchema });
 
   const onSubmit = handleSubmit(async ({ email, password }) => {
-    const {
-      data: { createUserSession: createdUserSession },
-    } = await createUserSession({ variables: { email, password } });
-    dispatch(setSession(createdUserSession));
+    await createUser({ variables: { email, password } });
+    reset();
+    pushChangeToLogin();
   });
 
   return (
@@ -78,23 +86,32 @@ const Login = ({onChangeToSignUp: pushChangeToSignUp}) => {
           ref={register}
         />
       </Label>
-      <LoginButton disabled={isSubmitting} type="submit">
-        Login
-      </LoginButton>{' '}
-      <OrSignUp>
+      <Label>
+        <LabelText>Confirm password</LabelText>
+        <TextInput
+          disabled={isSubmitting}
+          name="confirmPassword"
+          type="password"
+          ref={register}
+        />
+      </Label>
+      <SignUpButton disabled={isSubmitting || !isValid} type="submit">
+        Sign Up
+      </SignUpButton>{' '}
+      <OrLogin>
         or{' '}
         <a
           href="#"
           onClick={(event) => {
             event.preventDefault();
-            pushChangeToSignUp();
+            pushChangeToLogin();
           }}
         >
-          Sign up
+          Login
         </a>
-      </OrSignUp>
+      </OrLogin>
     </form>
   );
 };
 
-export default Login;
+export default SignUp;
